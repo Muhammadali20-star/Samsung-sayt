1b9a8f3f372a69b2c94664142b22a5a5ad766e02
Muhammadali <muhamadalimamadaminov@gmail.com>
2025-05-15 20:59:23 +0300

    hero jsx

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = ({title, desc, image, catigory, name}) => {
       <h2 className='text-5xl font-semibold'>{title}</h2>
       <div className='flex gap-6 justify-center mt-2'>
       {catigory.map((item, index) => (
-         <h3  key={index}>{item}</h3>
+         <h3 className='hover:underline decoration-black'  key={index}>{item}</h3>
       ))}
       </div>
         <div className='mt-90 mb-6'>
@@ -14,8 +14,8 @@ const Hero = ({title, desc, image, catigory, name}) => {
         </div>
         <p className='text-[14px] mb-4'>{desc}</p>
         <div className='flex gap-4 justify-center'>
-          <button className='underline text-black-300'>shop all appliance deals</button>
-          <button className='border border-black-600 text-black-600 px-4 py-2 rounded'>Buy Shop</button>
+          <button className='underline decoration-black text-black-300'>shop all appliance deals</button>
+          <button className=' bg-[#0B0B0B] rounded-[15px] hover:text-[#0B0B0B] hover:bg-white text-white px-4 py-2'>Buy Shop</button>
         </div>
     </div>
   )
